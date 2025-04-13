575feb1e48d1a2289c3a4287aef057e8f05dcf8b
TripleSM <50290618+TripleSM@users.noreply.github.com>
2025-04-13 18:09:36 +0530

    Update MinimalWndWPF.cs

--- MinimalWnd/MinimalWndWPF.cs
+++ MinimalWnd/MinimalWndWPF.cs
@@ -17,7 +17,14 @@ namespace MinimalWndWPF
             
             Window theWindow = new Window(); // Create the Window object.
             theWindow.Title = "Test Window App"; // Set the title.
+            
             theWindow.Content = strMessage; // Set the window content.
+
+            /*TextBlock textBlock1 = new TextBlock();
+            textBlock1.Text = strMessage;
+            textBlock1.VerticalAlignment = VerticalAlignment.Center;
+            textBlock1.HorizontalAlignment = HorizontalAlignment.Center;
+            theWindow.Content = textBlock1;*/
 
             Application theApp = new Application(); // Create an Application object.
             theApp.Run(theWindow); // Start application running.
